8e9f9d451a999aea86e1db259f1b05297713d8c5
Thomas von Deyen <tvd@magiclabs.de>
2014-12-19 18:28:46 +0100

    Updates assets paths in install generator.

--- lib/generators/spree_payone/install/install_generator.rb
+++ lib/generators/spree_payone/install/install_generator.rb
@@ -2,24 +2,37 @@ module SpreePayone
   module Generators
     class InstallGenerator < Rails::Generators::Base
       def add_javascripts
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_payone\n"
+        append_file "#{assets_folder}/javascripts/spree/backend/all.js",
+          "//= require spree_payone/admin\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_payone\n", :before => /\*\//, :verbose => true
+        inject_into_file "#{assets_folder}/stylesheets/spree/backend/all.css",
+          " *= require spree_payone/admin\n",
+          before: /\*\//, verbose: true
       end
 
       def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree_payone'
+        rake_task 'spree_payone:install:migrations'
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'bundle exec rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        res = ask "Would you like to run the migrations now? [Y/n]"
+        if res == "" || res.downcase == "y"
+          rake_task 'db:migrate'
+        else
+          say "Skipping rake db:migrate, don't forget to run it!", :yellow
+        end
+      end
+
+      private
+
+      def assets_folder
+        Rails.root.join("vendor/assets/")
+      end
+
+      def rake_task(name)
+        run "bundle exec rake #{name}"
       end
     end
   end
