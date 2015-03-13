27c3cbb7d5da5cd8604bf0589146117031e82edc
Thomas von Deyen <tvd@magiclabs.de>
2015-03-13 14:23:52 +0100

    Adds PAYONE tab into Spree main navi.

--- lib/spree_payone/engine.rb
+++ lib/spree_payone/engine.rb
@@ -1,35 +1,8 @@
 module SpreePayone
   class Engine < Rails::Engine
     engine_name 'spree_payone'
-    
-    config.autoload_paths += %W(#{config.root}/lib)
-    
-    # use rspec for tests
-    config.generators do |g|
-      g.test_framework :rspec
-    end
-    
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
-      
-      Deface::Override.new(
-        :virtual_path  => "spree/admin/configurations/index",
-        :insert_bottom => "[data-hook='admin_configurations_menu']",
-        :partial       => "spree/admin/configurations/index_payone_extension",
-        :name          => "index_payone_extension");
-      
-      Deface::Override.new(
-        :virtual_path  => "spree/admin/shared/_configuration_menu",
-        :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-        :partial       => "spree/admin/shared/configuration_menu_payone_extension",
-        :name          => "configuration_menu_payone_extension");
-    end
-    
-    config.to_prepare &method(:activate).to_proc
-    
-    initializer "spree_payone.register.payment_methods", :after => 'spree.register.payment_methods' do |app|
+
+    initializer "spree_payone.register.payment_methods", after: 'spree.register.payment_methods' do |app|
       app.config.spree.payment_methods += [
         Spree::Gateway::PAYONE::CreditCard,
         Spree::PaymentMethod::PAYONE::OnlineBankTransfer,
@@ -41,5 +14,24 @@ module SpreePayone
       ]
     end
 
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    config.to_prepare do
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
+
+      Deface::Override.new(
+        virtual_path:  "spree/layouts/admin",
+        insert_bottom: "#main-sidebar",
+        partial:       "spree/payone/admin/shared/payone_tab",
+        name:          "payone_extension_menu_tab",
+        original:      "82da3a16506fbf9567b0f2623bd56cd234473735")
+    end
   end
 end
