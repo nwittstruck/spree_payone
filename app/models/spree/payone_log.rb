8f52f8f1ce3ae18d802635940806aa409dbfe635
Thomas von Deyen <tvd@magiclabs.de>
2015-03-13 14:24:13 +0100

    Updates PAYONE logs listing for Spree 3

--- app/models/spree/payone_log.rb
+++ app/models/spree/payone_log.rb
@@ -4,5 +4,9 @@ module Spree
     # we must run logger within new connection to avoid changes to be rollbacked
     # for details see: state_machine documentation
     establish_connection Rails.env.to_sym
+
+    def self.log_levels
+      self.uniq.order('level ASC').pluck(:level)
+    end
   end
 end
